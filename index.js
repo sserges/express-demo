8da536661dccb902d3c98fd1dcc4588729a89002
sserges <agounkeserge@gmail.com>
2020-05-11 21:07:24 +0000

    Environments

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if (app.get('env') === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled...');
+}
 
 app.use(logger);
 app.use(authenticate);
