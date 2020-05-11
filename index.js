768d74bdfe753b4e444326de8f0ad8ffb446db48
sserges <agounkeserge@gmail.com>
2020-05-11 19:24:43 +0000

    Third-party Middleware

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Joi = require('joi');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const logger = require('./logger');
 const authenticate = require('./authenticate');
@@ -9,9 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.use(logger);
-
 app.use(authenticate);
 
 const courses = [
