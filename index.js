6d4ff85dc2be99bd5c61fff24b8f5549f4670cfe
sserges <agounkeserge@gmail.com>
2020-05-15 18:52:45 +0000

    Configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Joi = require('joi');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const config = require('config');
 
 const logger = require('./logger');
 const authenticate = require('./authenticate');
@@ -12,6 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
