2f52d6af830eb05f7674e2a03fb662bf6306e053
sserges <agounkeserge@gmail.com>
2020-05-28 10:35:41 +0000

    Patterns For Dealing With Asynchronous Code

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
-const express = require('express');
-const Joi = require('joi');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const config = require('config');
+// const express = require('express');
+// const Joi = require('joi');
+// const helmet = require('helmet');
+// const morgan = require('morgan');
+// const config = require('config');
 
-const courses = require('./routes/courses');
-const home = require('./routes/home');
+// const courses = require('./routes/courses');
+// const home = require('./routes/home');
 
-const startupDebugger = require('debug')('app:startup');
-const dbDebugger = require('debug')('app:db');
+// const startupDebugger = require('debug')('app:startup');
+// const dbDebugger = require('debug')('app:db');
 
-const logger = require('./middlewares/logger');
-const authenticate = require('./middlewares/authenticate');
+// const logger = require('./middlewares/logger');
+// const authenticate = require('./middlewares/authenticate');
 
-const app = express();
+// const app = express();
 
-app.set('view engine', 'pug');
-app.set('views', './views'); // default
+// app.set('view engine', 'pug');
+// app.set('views', './views'); // default
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(helmet());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static('public'));
+// app.use(helmet());
 
-app.use('/api/courses', courses);
-app.use('/', home);
+// app.use('/api/courses', courses);
+// app.use('/', home);
 
-// Configuration
-// console.log('Application Name: ' + config.get('name'));
-// console.log('Mail Server: ' + config.get('mail.host'));
-// console.log('Mail Password: ' + config.get('mail.password'));
+// // Configuration
+// // console.log('Application Name: ' + config.get('name'));
+// // console.log('Mail Server: ' + config.get('mail.host'));
+// // console.log('Mail Password: ' + config.get('mail.password'));
 
-if (app.get('env') === 'development') {
-  app.use(morgan('tiny'));
-  startupDebugger('Morgan enabled...');
-}
+// if (app.get('env') === 'development') {
+//   app.use(morgan('tiny'));
+//   startupDebugger('Morgan enabled...');
+// }
 
-// Db work...
-dbDebugger('Connected to the database...');
+// // Db work...
+// dbDebugger('Connected to the database...');
 
-app.use(logger);
-app.use(authenticate);
+// app.use(logger);
+// app.use(authenticate);
 
-// PORT
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+// // PORT
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+const getUser = (id) => {
+  setTimeout(() => {
+    console.log('Reading a user from a database...');
+    return { id, gitHubUsername: 'mosh' };
+  }, 2000);
+
+  return 1;
+};
+
+console.log('Before');
+const user = getUser(1);
+console.log(user);
+console.log('After');
