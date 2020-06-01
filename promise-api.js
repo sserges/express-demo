2b6af77811a0ef2e37e710a472d4fa147cf49b36
sserges <agounkeserge@gmail.com>
2020-06-01 18:58:44 +0000

    Running Parallel Promises

--- promise-api.js
+++ promise-api.js
@@ -1,5 +1,17 @@
-// const p = Promise.resolve({ id: 1 });
-// p.then((result) => console.log(result));
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('Async operation 1...');
+    resolve(1);
+  }, 2000);
+});
 
-const pr = Promise.reject(new Error('reason for rejection...'));
-pr.catch((error) => console.log(error));
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('Async operation 2...');
+    resolve(2);
+  }, 2000);
+});
+
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log('Error', err.message));
