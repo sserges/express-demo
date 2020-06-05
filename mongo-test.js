120e3ff9fa5375500f4a22963f1012ffce4f9b3d
sserges <agounkeserge@gmail.com>
2020-06-05 11:44:34 +0000

    Querying Documents

--- mongo-test.js
+++ mongo-test.js
@@ -13,8 +13,9 @@ const courseSchema = new mongoose.Schema({
   isPublished: Boolean,
 });
 
+const Course = mongoose.model('Course', courseSchema);
+
 const createCourse = async () => {
-  const Course = mongoose.model('Course', courseSchema);
   const course = new Course({
     name: 'Angular Course',
     author: 'Mosh',
@@ -26,4 +27,12 @@ const createCourse = async () => {
   console.log(result);
 };
 
-createCourse();
+const getCourses = async () => {
+  const courses = await Course.find({ author: 'Mosh', isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+};
+
+getCourses();
