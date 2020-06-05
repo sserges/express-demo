fab33e0aba62a981848969602f9e0dd8e1e43d2f
sserges <agounkeserge@gmail.com>
2020-06-05 10:23:20 +0000

    Models

--- mongo-test.js
+++ mongo-test.js
@@ -12,3 +12,11 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+  name: 'Node.js Course',
+  author: 'Mosh',
+  tags: ['node', 'backend'],
+  isPublished: true,
+});
