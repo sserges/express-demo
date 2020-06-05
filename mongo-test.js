050d04de96ab39fe3f41c7a07e3e447476ca9c42
sserges <agounkeserge@gmail.com>
2020-06-05 12:26:47 +0000

    Logical Query Operators

--- mongo-test.js
+++ mongo-test.js
@@ -28,18 +28,14 @@ const createCourse = async () => {
 };
 
 const getCourses = async () => {
-  // eq (equal)
-  // ne (not equal)
-  // gt (greater than)
-  // gte (greater than or equal to)
-  // lt (less than)
-  // lte (less than or equal to)
-  // in
-  // nin (not in)
+  // or
+  // and
+
   const courses = await Course
     // .find({ author: 'Mosh', isPublished: true })
-    // .find({price: { $gte: 10, $lte: 20 } })
-    .find({ price: { $in: [10, 15, 20] } })
+    .find()
+    .or([{ author: 'Mosh' }, { isPublished: true }])
+    .and([{ author: 'Mosh' }, { isPublished: true }])
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
