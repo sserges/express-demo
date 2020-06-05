3136d5c4dfcd24aff5996c12c759532924bb97cd
sserges <agounkeserge@gmail.com>
2020-06-05 18:18:00 +0000

    solution3

---
+++ solution3.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mongo-exercises');
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+const getCourses = async () => {
+  return await Course.find({
+    isPublished: true,
+  })
+    .or([{ price: { $gte: 15 } }, { name: /.*by.*/i }])
+    .sort('-price')
+    .select('name author price tags');
+};
+
+const run = async () => {
+  const courses = await getCourses();
+  console.log(courses);
+};
+
+run();
