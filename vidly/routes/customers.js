eb50a93d83a20e5da5f0bf752fea0d596224297a
sserges <agounkeserge@gmail.com>
2020-06-17 21:38:46 +0000

    Project- Build the Customers API

---
+++ vidly/routes/customers.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const express = require('express');
+const router = express.Router();
+
+const Customer = mongoose.model(
+  'Customer',
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+    isGold: {
+      type: Boolean,
+      default: false,
+    },
+    phone: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+  })
+);
+
+router.get('/', async (req, res) => {
+  const customers = await Customer.find().sort('name');
+  res.send(customers);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let customer = new Customer({
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold,
+  });
+  customer = await customer.save();
+
+  res.send(customer);
+});
+
+function validateCustomer(customer) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
+    isGold: Joi.boolean(),
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+module.exports = router;
